d56d4f506ec253b468b1571dd995f050ed395dba
Kasra <kkheiltash@yahoo.de>
2021-07-01 17:17:16 +0200

    Alien verfeinert

--- src/scripts/Alien.gd
+++ src/scripts/Alien.gd
@@ -1,23 +1,25 @@
 extends KinematicBody2D
 
-var move_speed = 7
+var move_speed = 15
 var hits = 2
 var shootingcount = rand_range(10, 70)
 
 
-
 func _physics_process(delta):
 	var collision = move_and_collide(Vector2.DOWN * delta * move_speed)
+	#print(position.y)
 	shootingcount +=delta
 	if global_position.y >= 0:
 		shoot()
-	if global_position.y == 100: #215.46
+	if global_position.y >= 245.46: #215.46
 		get_tree().reload_current_scene()
 	
 func kill():
 	hits -= 1
 	if hits == 0:
 		queue_free()
+		
+
 
 func shoot():
 	if shootingcount >= 40:
